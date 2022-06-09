3a84e2a8b3344f2cfb9ede545e35d29bdf501d5f
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-06-09 16:38:25 +0300

    commit tree 8be0ff2002754ae2de8132615ad706281de0297b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function superbowlWin(records) {
+    let year;
+    records.find(function (item) {
+        if (item.result === "W") {
+            //console.log(item.year)
+            return year = item.year
+        }
+        
+    })
+    return year
+    
+}
+superbowlWin
+/*
+function win (item) {
+    if (item.result === "W") {
+        return item.year;
+    }
+}
+*/
